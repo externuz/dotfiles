acdf101d50213cb8361a26ddf2b1f1b049c7d8c3
Vladislav Kolesnikov <externuzkz@gmail.com>
2024-11-25 23:54:01 +0500

    Create wezterm.linux.lua

---
+++ .config/wezterm/wezterm.linux.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE | TITLE",
+	color_scheme = "Tokyo Night Storm",
+	font = wezterm.font("Hack Nerd Font", { weight = "Regular" }),
+	font_size = 11,
+}
+
+return config
