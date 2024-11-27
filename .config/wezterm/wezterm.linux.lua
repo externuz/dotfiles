2f26676a1b87aad972fd013a804ea694fb166440
Vladislav Kolesnikov <externuzkz@gmail.com>
2024-11-27 23:55:49 +0500

    Update wezterm.linux.lua

--- .config/wezterm/wezterm.linux.lua
+++ .config/wezterm/wezterm.linux.lua
@@ -1,15 +1,36 @@
 local wezterm = require("wezterm")
 
-config = wezterm.config_builder()
+local config = {}
 
-config = {
-	automatically_reload_config = true,
-	enable_tab_bar = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE | TITLE",
-	color_scheme = "Tokyo Night Storm",
-	font = wezterm.font("Hack Nerd Font", { weight = "Regular" }),
-	font_size = 11,
-}
+-- Use config builder object if possible
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- Settings
+config.color_scheme = "Tokyo Night Storm"
+config.font = wezterm.font_with_fallback({
+	{ family = "Hack Nerd Font", scale = 1.5 },
+	{ family = "Courier New", scale = 1.5 },
+})
+config.window_background_opacity = 0.85
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.scrollback_lines = 2048
+config.default_workspace = "home"
+config.automatically_reload_config = true
+
+-- Tabs
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+
+-- Right status
+wezterm.on("update-right-status", function(window)
+	local time = wezterm.strftime("%-H:%M %p")
+
+	window:set_right_status(wezterm.format({
+		{ Text = wezterm.nerdfonts.md_clock .. " " .. time },
+	}))
+end)
 
 return config
